d01f2f8a02da91f1b894e0b81227527aa3754810
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-05-05 08:52:16 -0400

    bug: hide tags if empty string

--- src/lib/components/atoms/TagsBlock.svelte
+++ src/lib/components/atoms/TagsBlock.svelte
@@ -8,9 +8,10 @@ Display tags in a row.
 </script>
 
 <template lang="pug">
-	.inline-block.mr-4
-		+each('tags as tag')
-			span.inline-block.py-1.px-2.rounded.border-white.border.leading-none.ml-3.text-12(
-				class="border-[1px]"
-			) { tag }
+	+if('tags.length > 0 && tags[0] != ""')
+		.inline-block.mr-4
+			+each('tags as tag')
+				span.inline-block.py-1.px-2.rounded.border-white.border.leading-none.ml-3.text-12(
+					class="border-[1px]"
+				) { tag }
 </template>
