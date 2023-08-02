730a1f0e4a4f4e834419c49886063168a58558cc
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-08-02 14:29:23 -0400

    fix: style in mobile header

--- src/lib/components/organisms/Header.svelte
+++ src/lib/components/organisms/Header.svelte
@@ -20,20 +20,20 @@ Here's some documentation for this component.
 </script>
 
 <template lang="pug">
-	.relative.flex.justify-between.items-center.px-8.pt-6.pb-2.border-b.mb-8(
-		class="border-b-neutral-100/20"
+	.flex.justify-between.items-center.px-8.py-4.border-b(
+		class=" sm:relative border-b-neutral-100/20"
 	)
-		//- message
+		//- branding
 		.flex.items-center
 			.mr-2: a.tracking-widest.leading-none.underline-offset-4.text-accent(
 				class="pointer:cursor",
 				href="/"
 			) TOODS
-			//.mr-3.leading-none |
-			//.italic.font-light.font-serif.leading-none.opacity-80 { message }
 
-		div: a.text-13(
-			class="inline-block py-2 px-2 rounded outline-white outline leading-none",
-			href="/create"
-		) + Create Todo
+		//- action button
+		.flex-grow.flex.justify-end(class="sm:relative")
+			a.text-13(
+				class="inline-block py-2 px-2 rounded outline-white outline leading-none",
+				href="/create"
+			) + Create Todo
 </template>
