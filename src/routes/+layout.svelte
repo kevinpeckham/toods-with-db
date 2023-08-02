cbdfaee8b22d0d8a14a53529105e471ab32c3485
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-08-02 14:30:00 -0400

    refactor metadata insert

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
 	import "/src/app.css";
+
+	// import child page data
+	import { page } from "$app/stores";
+
+	// components
+	import Header from "$components/organisms/Header.svelte";
 </script>
 
 <template lang="pug">
+	//- head metadata
+	svelte:head
+		title { $page.data.metaTitle ?? "Sage Energy" }
+		+if('$page.data.metaDescription')
+			meta(
+				content!="{ $page.data.metaDescription }",
+				name="description"
+			)
+	Header
 	slot
 </template>
