3e5be27ae873be3f6ebbedc8696d739816bd5a0c
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-08-22 11:06:16 -0400

    add util for scrubbing desc text

--- src/lib/utils/scrubUtils.ts
+++ src/lib/utils/scrubUtils.ts
@@ -1,3 +1,15 @@
+export function scrubDescriptionString(str: string) {
+	// limit tag length to 80 characters
+	const limited = str?.slice(0, 80) ?? "";
+
+	// replace any invalid characters
+	// allow only a-z, hyphen, and colon
+	const replaced = limited.replace(/[^a-z1-9-:.,;?/()]/gi, "");
+
+	// return
+	return replaced;
+}
+
 export function scrubTagString(str: string) {
 	// limit tag length to 20 characters
 	const limited = str?.slice(0, 20) ?? "";
